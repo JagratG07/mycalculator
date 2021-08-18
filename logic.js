9295c3dc96da8ee242acecc49b49562d5330154a
JagratG07 <89118660+JagratG07@users.noreply.github.com>
2021-08-18 11:59:52 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,30 @@
+let screen = document.getElementById('screen');
+ buttons = document.querySelectorAll('button');
+ let screenvalue = " ";
+ for(item of buttons){
+     item.addEventListener('click',(e)=>{
+     buttontext = e.target.innerText;
+     console.log( "the button text is" ,buttontext);
+
+      
+     if(buttontext=='x'){
+         buttontext='*';
+
+         screenvalue += buttontext;
+         screen.value = screenvalue;}
+     
+     else if(buttontext=='='){
+         screen.value = eval(screenvalue);
+     }
+     else if(buttontext=='C'){
+         screenvalue = " ";
+         screen.value = screenvalue; }
+         
+         else{
+             screenvalue+=buttontext;
+             screen.value = screenvalue;
+         }
+
+    
+     })
+ }
